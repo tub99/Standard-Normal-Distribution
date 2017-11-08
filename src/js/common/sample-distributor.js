ae428abb5f87ac5ed1c950582a4ab03d60f0db93
soumya.ghosh <soumya.ghosh@learningmate.com>
2017-11-08 17:59:20 +0530

    formulae added

--- src/js/common/sample-distributor.js
+++ src/js/common/sample-distributor.js
@@ -2,3 +2,33 @@
  * This class will be containing the necessary formulas
  * for sample distribution like getMean/Standard Deviation,etc
  */
+
+function SampleDistributor(dataSet) {
+    var data = dataSet.data,
+        getIndividualStandardDeviation = function (observedVal, mean) {
+
+        },
+        getIndividualVariance = function (observedVal, mean) {
+            return Math.pow((observedVal - mean), 2) / data.length;
+        };
+    this.getMean = function () {
+        var sum = data.reduce(function (a, b) {
+            return a + b;
+        });
+        return sum / data.length;
+    };
+    this.getTotalVariance = function () {
+        var mean = this.getMean(),
+            sum = 0;
+        for (var i of data) {
+           sum += getIndividualVariance(data[i], mean);
+        }
+        return Math.sqrt(sum)
+    };
+    this.getStandardDeviation = function () {
+
+    };
+    this.generateProbablisticValue = function (observedVal) {
+
+    }
+}
--- src/js/graph/graph-manager.js
+++ src/js/graph/graph-manager.js
@@ -2,3 +2,6 @@
  * An abstraction over the graph class which communicates with client data
  * and parser. Main class from where all necessary calls are made.
  */
+function GraphManager(){
+    
+}
