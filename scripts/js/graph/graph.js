45c0a5edc5a1b22d6081dffe3c16b4cd107d5487
soumya.ghosh <soumya.ghosh@learningmate.com>
2017-11-09 19:01:57 +0530

    all constants  retrieved from configuration file

--- scripts/js/graph/graph.js
+++ scripts/js/graph/graph.js
@@ -3,72 +3,87 @@ function Graph() {
         initializeBoard = function (axisLimits, showGrid, isNavigable, isZoomable) {
             board = JXG.JSXGraph.initBoard('box', {
                 axis: false,
-                boundingbox: [-5, 0.5, 5, -0.1],
-                grid: false,
+                boundingbox: axisLimits,
+                grid: showGrid,
                 showCopyright: false,
-                showNavigation: false,
-                showZoom: false
+                showNavigation: isNavigable,
+                showZoom: isZoomable
             });
         },
         createXAxis = function () {
             xaxis = board.create('axis', [
                 [0, 0],
                 [1, 0]
-            ], {grid:false});
+            ], {
+                grid: false
+            });
         },
         ceateYAxis = function () {
             yaxis = board.create('axis', [
                 [0, 0],
                 [0, 1]
-            ], {grid: false});
+            ], {
+                grid: false
+            });
             yaxis.removeAllTicks();
         },
         generateFunctionGraph = function (graphConfigs) {
             var e = Math.E,
-                sd = graphConfigs.STANDARD_DEVIATION || 1;
-            var functionGraph = board.create('functiongraph', [function (z) {
+                sd = graphConfigs.STANDARD_DEVIATION || 1,
+                functionGraph = board.create('functiongraph', [function (z) {
                 fx = (1 / (Math.sqrt(2 * Math.PI) * sd)) * Math.pow(e, (-0.5 * (z * z)));
                 return fx;
             }], {
-                fillcolor: 'yellow',
-                fillopacity: 0.3,
+                fillcolor: graphConfigs.graph.cosmetics.color,
+                fillopacity: graphConfigs.graph.cosmetics.opacity,
                 highlight: false
             });
-           
+
             return functionGraph;
         },
         getScrubberCoords = function (e) {
-            var i = 0;
-            var cPos = board.getCoordsTopLeftCorner(e, i),
+            var i = 0,
+                cPos = board.getCoordsTopLeftCorner(e, i),
                 absPos = JXG.getPosition(e, i),
                 dx = absPos[0] - cPos[0],
                 dy = absPos[1] - cPos[1];
 
             return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
         },
+        getArea = function (x) {
+            // TODO: get val from configs
+            var sd = 1;
+            var areaFirstHalf = (1 / (Math.sqrt(2 * Math.PI) * sd)) * Math.pow(Math.E, (-0.5 * (x * x))),
+                areaSecondHalf = 1 - areaFirstHalf;
+            return {
+                presentValue : x,
+                leftArea: areaFirstHalf,
+                rightArea: areaSecondHalf
+            };
+        },
         attatchEventsToScrubber = function (scrubberPoint) {
             scrubberPoint.on('drag', function (evt) {
-                 if(scrubberPoint.position <= -3.5) {
+                let area = getArea(scrubberPoint.position);
+                console.log(area);
+                if (scrubberPoint.position <= -3.5) {
                     // returning false or stop event is not working
                     // hence setting to extreme positions 
                     scrubberPoint.position = -3.5
-                 } else if(scrubberPoint.position>=3.5) scrubberPoint.position = 3.5
+                } else if (scrubberPoint.position >= 3.5) scrubberPoint.position = 3.5
             })
-          },
-        getArea = function () {
-
         },
+
 
         generateScrubber = function (curve, scrubberPos) {
             var integralCurve = board.create('integral', [
-                [1, 4.0], curve
+               scrubberPos, curve
             ], {
                 withLabel: false,
                 isDraggable: false,
-                curveLeft : {
-                    showInfoBox : false
+                curveLeft: {
+                    showInfoBox: false
                 },
-                curveRight : {
+                curveRight: {
                     visible: false
                 }
             });
@@ -83,7 +98,7 @@ function Graph() {
         if (axisObj.hasYAxis) ceateYAxis();
         var functionGrpah = generateFunctionGraph(graphConfigs);
         if (hasScrubber) {
-            generateScrubber(functionGrpah);
+            generateScrubber(functionGrpah, graphConfigs.graph.area.scrubberPos);
         }
     }
     var resize = function () {
